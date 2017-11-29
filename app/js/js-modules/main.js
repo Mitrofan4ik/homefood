8f5b5e7989217b11cc1c967e37979986f40470cf
NIX\mytrofanova <ekaterinamytrofanova@nixsolutions.com>
2017-11-29 17:08:51 +0200

    add slider categories

--- app/js/js-modules/main.js
+++ app/js/js-modules/main.js
@@ -1,4 +1,15 @@
 //= js-assets/functions
 
+$(document).ready(function() {
+
+  var option1 = {
+    infinite: true,
+    slidesToShow: 3,
+    speed: 300,
+    slidesToScroll: 1
+  }
+  $(".jsCategories").slick(option1);
+});
+
 
 
