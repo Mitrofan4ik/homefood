c5a5f4efdee0da85008a258b563d6cc906e66441
NIX\mytrofanova <ekaterinamytrofanova@nixsolutions.com>
2017-11-30 13:13:00 +0200

    add new chef block markup

--- app/js/js-modules/main.js
+++ app/js/js-modules/main.js
@@ -8,8 +8,48 @@ $(document).ready(function() {
     speed: 300,
     slidesToScroll: 1
   }
+
   $(".jsCategories").slick(option1);
+
+
+
+  $(".jsChefBox").slick('unslick');
+
+  // rating stars
+  var starList = $(".jsStarLIst");
+  var stars = $(".jsStarItem");
+  starList.on("click", ".jsStarItem", function() {
+    var $this = $(this);
+    stars.removeClass("selected");
+    stars.removeClass("secondary");
+    $this.addClass("selected");
+    $this.prevAll().addClass("secondary");
+  });
+
+  chefSlider();
 });
+
+$(window).resize(function() {
+  chefSlider();
+});
+
+function chefSlider() {
+  var windowWidth = $(window).width();
+  var chefBox = $(".chef-box");
+  if (windowWidth <= 1200) {
+    chefBox.addClass("jsChefBox");
+    $(".jsChefBox").slick({
+       slidesToShow: 4,
+    });
+  } else {
+    chefBox.removeClass("jsChefBox");
+    $(".jsChefBox").slick("unslick");
+  }
+}
+
+
+
+
 
 
 
