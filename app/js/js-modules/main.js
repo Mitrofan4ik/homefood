246af9cddc2dac64591f221ca5b184d45503c553
NIX\mytrofanova <ekaterinamytrofanova@nixsolutions.com>
2017-11-30 19:10:56 +0200

    add fixed header

--- app/js/js-modules/main.js
+++ app/js/js-modules/main.js
@@ -2,14 +2,57 @@
 
 $(document).ready(function() {
 
-  var option1 = {
+  var optionCategoriesSlider = {
     infinite: true,
     slidesToShow: 3,
     speed: 300,
     slidesToScroll: 1
   }
+  var optionChefSlider = {
+    slidesToShow: 4,
+    variableWidth: true,
+    unslick: true,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+       breakpoint: 1200,
+       settings: {
+         dots: true,
+         infinite: false,
+         unslick: false,
+         slidesToShow: 3,
+         variableWidth: true,
+         slidesToScroll: 1
+        }
+      }
+    ]
+  }
+  // var optionCustomerSlider = {
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   variableWidth: true,
+  //   // unslick: true,
+  //   dots: false,
+  //   arrows: false,
+  //   responsive: [
+  //     {
+  //      breakpoint: 1200,
+  //      settings: {
+  //        dots: true,
+  //        infinite: false,
+  //        unslick: false,
+  //        slidesToShow: 3,
+  //        variableWidth: true,
+  //        slidesToScroll: 1
+  //       }
+  //     }
+  //   ]
+  // }
 
-  $(".jsCategories").slick(option1);
+  $(".jsCategories").slick(optionCategoriesSlider);
+  $(".jsChefBox").slick(optionChefSlider);  
+  // $(".jsCustomer").slick();
 
   // rating stars
   var starList = $(".jsStarList");
@@ -23,30 +66,20 @@ $(document).ready(function() {
     $this.prevAll().addClass("secondary");
   });
 
- $(".jsChefBox").slick({
-   slidesToShow: 4,
-   variableWidth: true,
-   unslick: true,
-   dots: false,
-   arrows: false,
-   responsive: [
-      {
-       breakpoint: 1200,
-       settings: {
-         dots: true,
-         infinite: false,
-         unslick: false,
-         slidesToShow: 3,
-         variableWidth: true,
-         slidesToScroll: 1
-        }
-      }
-    ]
-  });  
 });
 
-$(window).resize(function() {
+// $(window).resize(function() {
 
+// });
+//Animated header
+$(window).on('scroll', function() {
+    var scroll = $(window).scrollTop();
+    var windowWidth = $(window).width();
+    if (scroll >= 50 && windowWidth >= 1570) {
+      $('#header').addClass('fixed');
+    } else {
+      $('#header').removeClass('fixed');
+    }
 });
 
 
